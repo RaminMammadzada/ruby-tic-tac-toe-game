82cc92950224522d46860ea6d8213715641a87ab
Sajjad Ahmad <jogimar14@gmail.com>
2020-08-20 02:36:18 +0500

    Add the hash game_board to store user moves and print board

--- bin/main.rb
+++ bin/main.rb
@@ -1,38 +1,32 @@
+def print_board(board)
+    print board['1'] + "|" + board['2']+ "|" + board['3']
+    puts
+    puts"--+-+--"
+    print board['4'] + "|" + board['5']+ "|" + board['6']
+    puts
+    puts"--+-+--"
+    print board['7'] + "|" + board['8']+ "|" + board['9']
+ end
 
+puts "Please enter player1 name: "
+player_1 = gets.chomp
+puts "#{player_1}"
 
+puts "Please enter player2 name: "
+player_2 = gets.chomp
+puts "#{player_2}"
 
+game_board = {'1' => ' ', '2' => ' ', '3'=> ' ',
+              '4' => ' ', '5' => ' ', '6'=> ' ',
+              '7' => ' ', '8' => ' ', '9'=> ' '
+              }
 
-puts "Please enter the player 1 name:"
-player1 = gets.chomp
-puts player1
+puts "Player1 please enter location(1-9) to puts x there!"
+player1_move = gets.chomp
+game_board[player1_move] = 'x'
+print_board(game_board)
 
-puts ""
-
-puts "Please enter the player 2 name:"
-player2 = gets.chomp
-puts player2
-
-puts ""
-
-puts "...::: THE GAME IS STARTED :::..."
-puts "It is #{player1}'s term to play now.."
-puts "Please enter the location by typing integers between 1-9 where you want to put your X sign."
-player1Move = gets.chomp
-puts " | | "
-puts "- - -"
-puts " | | "
-puts "- - -"
-puts " | | "
-
-puts ""
-puts ""
-
-puts "It is #{player2}'s term to play now.."
-puts "Please enter the location by typing integers between 1-9 where you want to put your O sign."
-player1Move = gets.chomp
-puts " | | "
-puts "- - -"
-puts " | | "
-puts "- - -"
-puts " | | "
-
+puts "Player2 please enter location(1-9) to puts o there!"
+player2_move = gets.chomp
+game_board[player2_move] = 'o'
+print_board(game_board)
