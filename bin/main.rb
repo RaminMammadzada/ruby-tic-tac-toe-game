4bfb678ec5c23c33a0a606c0fea934f508fd518a
Ramin Mammadzada <remki-design@Hotmail.com>
2020-08-19 23:00:42 +0300

    Add simple print statements for check.

--- bin/main.rb
+++ bin/main.rb
@@ -1,1 +1,38 @@
-puts 'Hello world!'
+
+
+
+
+puts "Please enter the player 1 name:"
+player1 = gets.chomp
+puts player1
+
+puts ""
+
+puts "Please enter the player 2 name:"
+player2 = gets.chomp
+puts player2
+
+puts ""
+
+puts "...::: THE GAME IS STARTED :::..."
+puts "It is #{player1}'s term to play now.."
+puts "Please enter the location by typing integers between 1-9 where you want to put your X sign."
+player1Move = gets.chomp
+puts " | | "
+puts "- - -"
+puts " | | "
+puts "- - -"
+puts " | | "
+
+puts ""
+puts ""
+
+puts "It is #{player2}'s term to play now.."
+puts "Please enter the location by typing integers between 1-9 where you want to put your O sign."
+player1Move = gets.chomp
+puts " | | "
+puts "- - -"
+puts " | | "
+puts "- - -"
+puts " | | "
+
