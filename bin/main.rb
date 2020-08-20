76f154270898cd8b8060365c368ced37706f814c
Ramin Mammadzada <remki-design@Hotmail.com>
2020-08-20 19:05:22 +0300

    Fix style issues shown by rubocop linter.

--- bin/main.rb
+++ bin/main.rb
@@ -2,36 +2,35 @@
 require '../lib/Player'
 require '../lib/Game'
 
-puts "Please enter player1 name: "
+puts 'Please enter player1 name: '
 player_1_name = gets.chomp
-player_1 = Player.new(player_1_name)
+player1 = Player.new(player_1_name)
 
 
-puts "Please enter player2 name: "
+puts 'Please enter player2 name: '
 player_2_name = gets.chomp
-player_2 = Player.new(player_2_name)
+player2 = Player.new(player_2_name)
 
-game = Game.new(player_1, player_2)
+game = Game.new(player1, player2)
 
-puts "Welcome to the Tic - Tac - Toe game #{player_1.name} and #{player_2.name}"
+puts 'Welcome to the Tic - Tac - Toe game #{player1.name} and #{player2.name}'
 
-4.times do ||
-  puts "It is #{player_1.name}'s turn to play now."
-  puts "#{player_1.name}, please enter location(1-9) to puts x there!"
+4.times do
+  puts "It is #{player1.name}'s turn to play now."
+  puts '#{player1.name}, please enter location(1-9) to puts x there!'
   player1_move = gets.chomp
-  game.update_board(player1_move, player_1)
-  game.print_board()
+  game.update_board(player1_move, player1)
+  game.print_board
 
-  puts
+  puts ''
 
-  puts"It is #{player_2.name}'s turn to play now."
-  puts "#{player_2.name}, please enter location(1-9) to puts x there!"
+  puts "It is #{player2.name}'s turn to play now."
+  puts "#{player2.name}, please enter location(1-9) to puts x there!"
   player2_move = gets.chomp
-  game.update_board(player2_move, player_2)
+  game.update_board(player2_move, player2)
   game.print_board()
 end
 
-puts ""
+puts ''
 
-puts "Congrats! #{player_1.name} is a winner!"
-
+puts "Congrats! #{player1.name} is a winner! "
