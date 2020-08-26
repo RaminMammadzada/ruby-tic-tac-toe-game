979e9b65dd54f50d13f27c372c763e4239be6502
Sajjad Ahmad <jogimar14@gmail.com>
2020-08-27 03:48:24 +0500

    Update main and Game

--- bin/main.rb
+++ bin/main.rb
@@ -1,31 +1,24 @@
 require_relative '../lib/Player'
 require_relative '../lib/Game'
-
 puts 'Please enter player1 name: '
 player_1_name = gets.chomp
 player1 = Player.new(player_1_name, 'x')
-
 puts 'Please enter player2 name: '
 player_2_name = gets.chomp
 player2 = Player.new(player_2_name, 'o')
-
 game = Game.new(player1, player2)
-
 puts "Welcome to the Tic - Tac - Toe game #{player1.name} and #{player2.name}"
 game.board.print_board
 loop do
   game.move(player1)
   break if game.who_won != ''
-
   puts ''
   puts '- - - - - - - -'
   game.move(player2)
   break if game.who_won != ''
 end
-
 if game.who_won == 'draw'
   puts "It's a draw"
 else
-  puts "Congratulations!   \
-        The winner is #{player1.is_winner ? player1.name : player2.name} !"
+  puts 'Congratulations! ' + "The winner is #{game.winner}"
 end
--- lib/game.rb
+++ lib/game.rb
@@ -1,3 +1,4 @@
+# rubocop:disable Style/GuardClause,Metrics/CyclomaticComplexity,Metrics/AbcSize,Metrics/MethodLength:
 require_relative './Board'
 # This is a class which all of the methods about user interface will be in
 class Game
@@ -6,8 +7,8 @@ class Game
     @player1 = player1
     @player2 = player2
     @board = Board.new(@player1, @player2)
+    @winner = ''
   end
-
   def input_is_valid?(user_move)
     if user_move.to_i >= 1 && user_move.to_i <= 9
       if @board.blank?(user_move)
@@ -20,38 +21,38 @@ class Game
       false
     end
   end
-
   def any_empty?
     @board.board.values.any?(' ')
   end
-
   def move(player)
     puts "It is #{player.name}'s turn to play now."
     puts "#{player.name}, please enter location(1-9) to puts x there!"
     player_move = ''
-
     loop do
       player_move = gets.chomp
       break if input_is_valid?(player_move)
-
       puts 'Please enter the valid number as explained above!'
     end
-
     @board.update_board(player_move, player)
     @board.print_board
   end
-
   def who_won
     check_diagonal
     check_horizontal
     check_vertical
-    @player1.name if @player1.is_winner
-    @player2.name if @player2.is_winner
-    return '' unless @board.any_empty?
-
-    'draw'
+    if @player1.is_winner
+      @winner = @player1.name
+      @player1.name
+    elsif @player2.is_winner
+      @winner = @player2.name
+      @player2.name
+    elsif @board.any_empty?
+      ''
+    else
+      @winner = 'draw'
+      'draw'
+    end
   end
-
   def check_horizontal
     horizontal1 = [@board.board['1'], @board.board['2'], @board.board['3']]
     horizontal2 = [@board.board['4'], @board.board['5'], @board.board['6']]
@@ -60,13 +61,13 @@ class Game
        horizontal2.all?(@player1.tag) || \
        horizontal3.all?(@player1.tag)
       @player1.is_winner = 1
-    elsif horizontal1.all?(@player2.tag) || \
+    end
+    if horizontal1.all?(@player2.tag) || \
        horizontal2.all?(@player2.tag) || \
        horizontal3.all?(@player2.tag)
       @player2.is_winner = 1
     end
   end
-
   def check_vertical
     vertical1 = [@board.board['1'], @board.board['4'], @board.board['7']]
     vertical2 = [@board.board['2'], @board.board['5'], @board.board['8']]
@@ -75,22 +76,22 @@ class Game
        vertical2.all?(@player1.tag) || \
        vertical3.all?(@player1.tag)
       @player1.is_winner = 1
-    elsif vertical1.all?(@player2.tag) || \
+    end
+    if vertical1.all?(@player2.tag) || \
        vertical2.all?(@player2.tag) || \
        vertical3.all?(@player2.tag)
       @player2.is_winner = 1
     end
   end
-
   def check_diagonal
     diagonal1 = [@board.board['1'], @board.board['5'], @board.board['9']]
     diagonal2 = [@board.board['3'], @board.board['5'], @board.board['7']]
-    if diagonal1.all?(@player1.tag) || \
-       diagonal2.all?(@player1.tag)
+    if diagonal1.all?(@player1.tag) || diagonal2.all?(@player1.tag)
       @player1.is_winner = 1
-    elsif diagonal1.all?(@player2.tag) || \
-       diagonal2.all?(@player2.tag)
+    end
+    if diagonal1.all?(@player2.tag) || diagonal2.all?(@player2.tag)
       @player2.is_winner = 1
     end
   end
 end
+# rubocop:enable Style/GuardClause,Metrics/CyclomaticComplexity,Metrics/AbcSize,Metrics/MethodLength:
