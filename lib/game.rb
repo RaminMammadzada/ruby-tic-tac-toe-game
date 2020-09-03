63f85ab5dac7e4513b8db345afc073a2b9846008
Ramin Mammadzada <remki-design@Hotmail.com>
2020-09-04 00:31:27 +0300

    Update board and game classes to suit with test.

--- lib/game.rb
+++ lib/game.rb
@@ -1,7 +1,7 @@
 require_relative './board'
 # This is a class which all of the methods about user interface will be in
 class Game
-  attr_reader :winner, :board, :player1, :player2
+  attr_reader :winner, :board
   def initialize(player1, player2)
     @player1 = player1
     @player2 = player2
@@ -16,7 +16,6 @@ class Game
       if @board.blank?(user_move)
         true
       else
-        Main.inform_user('nonblank_input')
         false
       end
     else
@@ -31,15 +30,13 @@ class Game
   public
 
   def move(player)
-    Main.inform_user('turn_info', player)
     player_move = ''
     loop do
       player_move = gets.chomp
       break if input_is_valid?(player_move)
 
-      Main.inform_user('invalid_input')
     end
-    @board.update_board(player_move, player)
+    @board.update_board(player_move, player.tag)
     @board.print_board
   end
 
@@ -67,7 +64,7 @@ class Game
     if line1.all?(player.tag) || \
        line2.all?(player.tag) || \
        line3.all?(player.tag)
-      player.is_winner = 1
+      player.is_winner = true
     end
   end
 
